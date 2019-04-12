013f2a40a63c233dc732d9b5a2e32375dcbf0faa
Ansh Singh <anshsingh.14.lko@gmail.com>
2019-04-12 21:18:31 +0530

    Create lineage.mk

---
+++ lineage.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2017 The LineageOS Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Device
+$(call inherit-product, device/oppo/17061/device.mk)
+
+# A/B updater
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+# Device identifiers
+PRODUCT_DEVICE := 17061
+PRODUCT_NAME := lineage_17061
+PRODUCT_BRAND := oppo
+PRODUCT_MODEL := Oppo Realme 1
+PRODUCT_MANUFACTURER := Oppo
+PRODUCT_RELEASE_NAME := 17061
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRODUCT_NAME=17061
+
+BUILD_FINGERPRINT := full_oppo6771_17061-user 8.1.0 O11019 1550589611 release-keys
