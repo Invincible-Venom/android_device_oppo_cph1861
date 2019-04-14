e49fc6da13caec7abd7cc4c7bcfe15f7ce6d2cb7
Ansh Singh <anshsingh.14.lko@gmail.com>
2019-04-14 09:11:30 +0530

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,14 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
-# Device init scripts
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.6771
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.6771
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
