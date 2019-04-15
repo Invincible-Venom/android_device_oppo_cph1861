625e4fc21280c4d75c4ebc4545623e4bfae49bf4
Ansh Singh <anshsingh.14.lko@gmail.com>
2019-04-15 16:19:05 +0530

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
