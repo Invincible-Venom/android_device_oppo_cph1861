792cdbd817e6ae69432c3d550eab2040e0209057
Ansh Singh <anshsingh.14.lko@gmail.com>
2019-04-15 06:48:44 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit device configuration
