c33de29fadcb0de2e0abbffcb87b4036114bda59
Ansh Singh <anshsingh.14.lko@gmail.com>
2020-04-05 20:30:06 +0530

    Update releasetools.py
    Add OTA Support

--- releasetools.py
+++ releasetools.py
@@ -32,6 +32,6 @@ def AddTrustZoneAssertion(info, input_zip):
   if m:
     versions = m.group(1).split('|')
     if len(versions) and '*' not in versions:
-      cmd = 'assert(CPH1859.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
+      cmd = 'assert(CPH1859,CPH1861.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
       info.script.AppendExtra(cmd)
   return
